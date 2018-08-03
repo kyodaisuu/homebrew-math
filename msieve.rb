36cd6524f1425ca4b1d78d18cc4bb8c1f4ef0e1a
kyodaisuu <kyodaisuu@users.noreply.github.com>
2018-08-04 02:18:05 +0900

    msieve 1.53

---
+++ msieve.rb
@@ -0,0 +1,18 @@
+class Msieve < Formula
+  desc "Factor large integers"
+  homepage "https://sourceforge.net/projects/msieve/"
+  url "https://downloads.sourceforge.net/projects/msieve/files/msieve/Msieve%20v1.53/msieve153_src.tar.gz"
+  sha256 "c5fcbaaff266a43aa8bca55239d5b087d3e3f138d1a95d75b776c04ce4d93bb4"
+
+  depends_on "gcc"
+
+  def install
+    system "make", "all"
+    mkdir bin
+    bin.install "msieve"
+  end
+
+  test do
+    system "#{bin}/msieve", "-q", "12"
+  end
+end
