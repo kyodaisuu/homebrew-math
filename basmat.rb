63a6d16aa2de66111a8757c9c63bf8d5771cf433
kyodaisuu <kyodaisuu@users.noreply.github.com>
2018-08-24 23:01:02 +0900

    basmat 3.2

--- basmat.rb
+++ basmat.rb
@@ -3,8 +3,8 @@ require "formula"
 class Basmat < Formula
   desc "Bashicu Matrix Calculator"
   homepage "https://kyodaisuu.github.io/basmat/"
-  url "https://github.com/kyodaisuu/basmat/archive/v3.1.tar.gz"
-  sha256 "4db441419d9d2f05e50001b06592b42d1f173ff28c002831e53ec6108fdac292"
+  url "https://github.com/kyodaisuu/basmat/archive/v3.2.tar.gz"
+  sha256 "fe9f954dd9cabdc5bc5d3e8eb4f2d4ac82799d2b96b476738bab309c36a28a38"
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
     system "make", "install"
