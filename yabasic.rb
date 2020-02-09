2740965f045c5af87acdca038c320426c07d7e41
kyodaisuu <kyodaisuu@users.noreply.github.com>
2020-02-10 02:09:46 +0900

    2.86.4

--- yabasic.rb
+++ yabasic.rb
@@ -3,8 +3,8 @@ require "formula"
 class Yabasic < Formula
   desc "Yet another BASIC"
   homepage "http://www.yabasic.de/"
-  url "http://www.yabasic.de/download/yabasic-2.79.2.tar.gz"
-  sha256 "9a819c570fe3986f14ec6b718f6eafb7caa627d192e22a865dcfe50b0ae2b175"
+  url "http://www.yabasic.de/download/yabasic-2.86.4.tar.gz"
+  sha256 "0287fca8bfb38005ba723e3615cfeae19e2625bec87a87bb01a02897a30706a9"
   def install
     ENV['CC'] = '/usr/bin/gcc -I/opt/X11/include'
     system "./configure", "--prefix=#{prefix}"
